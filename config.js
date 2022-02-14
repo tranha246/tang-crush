a808df860637189b4557a1f3cd6521947ed918d0
tranha246 <85277380+tranha246@users.noreply.github.com>
2022-02-14 21:26:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
-    introTitle: 'thùy dương à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'trúc quỳnh à😘!',
+    introDesc: `Trái đất vốn lạ thường😒
+    Mà sao em cứ đi nhầm đường😢
+    Lạc vào tim anh lẻ loi😊
+    Đằng sau chữ yêu đây là thương❤️`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
